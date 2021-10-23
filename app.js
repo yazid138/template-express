41edf2769c6449e45d4a262f59892c77ef8e2cc1
yazid138 <ma39isy@gmail.com>
2021-10-23 11:07:57 +0700

    template express v2

--- app.js
+++ app.js
@@ -14,32 +14,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
 
-const routes = require("./routes");
+const routes = require("./routes/index");
 
 routes.forEach((e) => {
 	require("./routes/" + e + ".routes")(app);
 });
-// catch 404 and forward to error handler
+// catch 404 and forward to error handlser
 
 app.use(function (req, res, next) {
 	res.status(404).json({
 		status: 404,
-		message: "page not found",
+		message: "Page Not Found",
 	});
 	// next(createError(404));
 });
 
 // error handler
-// app.use(function (err, req, res, next) {
-// 	// set locals, only providing error in development
-// 	res.locals.message = err.message;
-// 	res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// 	// render the error page
-// 	res.status(err.status || 500);
-// 	res.render("error");
+//   // render the error page
+//   res.status(err.status || 500);
 // });
 
 module.exports = app;
--- routes/mahasiswa.routes.js
+++ routes/mahasiswa.routes.js
@@ -3,6 +3,7 @@ const controllers = require("../controllers/index");
 
 module.exports = (app) => {
 	router.get("/", controllers.mahasiswa.getAll);
+	router.post("/", controllers.mahasiswa.create);
 
 	app.use("/api/v1/mahasiswa", router);
 };
