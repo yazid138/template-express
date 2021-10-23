dd7102fac8e4c64f109679f4b7472911bf9a5cbb
yazid138 <ma39isy@gmail.com>
2021-10-23 16:19:41 +0700

    template express v2.1.1

--- routes/mahasiswa.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const controllers = require("../controllers/index");
-
-module.exports = (app) => {
-	router.get("/", controllers.mahasiswa.getAll);
-	router.post("/", controllers.mahasiswa.create);
-
-	app.use("/api/v1/mahasiswa", router);
-};
